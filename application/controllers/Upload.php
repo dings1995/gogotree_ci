072a20e052b77683f14c5e9d775b0c7394dc7d4f
Suen Yuen <dings1995@gmail.com>
2018-01-12 00:33:21 +0800

    change base url link

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -14,7 +14,7 @@ class Upload extends CI_Controller {
     {
         $config['upload_path']      = './images/';
         $config['allowed_types']    = 'gif|jpg|png';
-        $config['max_size']     = 2048;
+        $config['max_size']     = 5000;
         $config['max_width']        = 1024;
         $config['max_height']       = 768;
         $config['encrypt_name'] = true;
